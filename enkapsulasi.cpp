2f2da6406e365da4f5018506b5969719cd7bc7f1
AryaGGWP <arya.lampung.2016@gmail.com>
2025-06-02 18:30:43 +0700

    define class and member with value

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class remoteLampu
+{
+    private:
+        string saklarNo[10];
+
+    public:
+        // Method untuk mengisi saklar
+        void setSaklarNo(int i, string value)
+        {
+           saklarNo[i] = value;
+        }
+
+};
